e07abc90797d1379a149fab896d902562737f706
Enrico Viceconti <dev.eviceconti@gmail.com>
2018-07-18 14:06:00 -0300

    added a check in the form not allowing duplicate items in the list

--- src/app/add-item/add-item.component.ts
+++ src/app/add-item/add-item.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 import { ListItemsService } from '../list-items.service';
 
@@ -9,16 +9,29 @@ import { ListItemsService } from '../list-items.service';
   styleUrls: ['./add-item.component.css']
 })
 export class AddItemComponent implements OnInit {
-  
+
   public newItemForm: FormGroup = new FormGroup({
     newItem: new FormControl(null)
   });
 
   //method will be called on user click
   public handleSubmit() {
-    this.listItemsService.addItem(this.newItemForm.value.newItem);
+    let item = this.newItemForm.controls.newItem.value;
+    let items = this.listItemsService.items;
+    let match = [];
+    
+    //check if the array is not empty and if item already exists in the array, before push it as a new item
+    if (items) {
+      match = items.filter(i => i === item);
+      //console.log(match);
+    }
+
+    if (match.length === 0) {
+      this.listItemsService.addItem(item);
+    } else {
+      alert(`O item ${item} ja esta na lista`);
+    }
     this.newItemForm.reset();
-    console.log(this.newItemForm.value.newItem);
   }
 
   constructor(private listItemsService: ListItemsService) { }
