8beb3a81a48dd5b43ff1b8c9befa9e4c807d0f69
Enrico Viceconti <dev.eviceconti@gmail.com>
2018-07-18 12:02:16 -0300

    Created a new Form, using ReactiveForms in the add-item component.

--- src/app/add-item/add-item.component.ts
+++ src/app/add-item/add-item.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-add-item',
@@ -6,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-item.component.css']
 })
 export class AddItemComponent implements OnInit {
+  
+  public newItemForm: FormGroup = new FormGroup({
+    newItem: new FormControl(null)
+  });
+
+  //method will be called on user click
+  public handleSubmit() {
+    console.log(this.newItemForm.value.newItem);
+  }
 
   constructor() { }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
@@ -16,7 +17,8 @@ import { TickedListComponent } from './ticked-list/ticked-list.component';
     TickedListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
