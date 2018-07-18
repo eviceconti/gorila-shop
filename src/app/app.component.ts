76b70fcd730c963caa905e6fdb7cbf090e3b63db
Enrico Viceconti <dev.eviceconti@gmail.com>
2018-07-18 20:52:49 -0300

    installed firebase and imported/started it on app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  ngOnInit() {
+    // Initialize Firebase
+    var config = {
+      apiKey: "AIzaSyAPG_6WlIYifjI0g8L5ttgWIQZfRvn4Diw",
+      authDomain: "app-gorila-shop.firebaseapp.com",
+      databaseURL: "https://app-gorila-shop.firebaseio.com",
+      projectId: "app-gorila-shop",
+      storageBucket: "app-gorila-shop.appspot.com",
+      messagingSenderId: "1029555916550"
+    };
+    firebase.initializeApp(config);
+  }
 }
